59e8999553d805ea75403a1fc66ffb96a93fcd0a
Abdullah <abdullahnazeer22@gmail.com>
2022-11-24 18:44:31 +0000

    methods-complete

--- src/main/java/com/sparta/Main.java
+++ src/main/java/com/sparta/Main.java
@@ -6,7 +6,17 @@ public class Main {
 
         PalindromeChecker checker = new PalindromeChecker();
 
-        checker.checkString("monsoon");
+        // checking if a word is a palindrome (case-sensitive)
+        checker.checkStringWord("noon");
+
+        // checking if a string is a palindrome (not case-sensitive, spaces are considered)
+        checker.checkStringNotCaseSensitive("! no!on !");
+
+        // checking if a string is a palindrome (not case-sensitive, spaces are considered)
+        checker.checkStringNotCaseSensitive("Noon");
+
+        // checking if an array of strings contains any palindromes (not case-sensitive, spaces are considered)
+        checker.checkStringArray(new String[]{"Noon", "Abdullah", "radar", "reviver", "lollipop", "civic", "s civic s"});
 
     }
 }
--- src/main/java/com/sparta/PalindromeChecker.java
+++ src/main/java/com/sparta/PalindromeChecker.java
@@ -1,18 +1,69 @@
 package com.sparta;
 
+import java.util.Arrays;
+
 public class PalindromeChecker {
 
-    public String checkString(String string) {
-
-        String result = "";
+    public void checkStringWord(String string) {
 
         String reversedString = "";
 
-        for (int i = 0; i < string.length(); i++) {
+        for (int i = string.length() - 1; i >= 0; i--) {
+            reversedString += string.charAt(i);
+        }
+
+        System.out.println(reversedString);
+
+        if (string.equals(reversedString)) {
+            System.out.println("This string is a palindrome!");
+        } else {
+            System.out.println("This string is not a palindrome!");
+        }
+
+    }
+
+    public void checkStringNotCaseSensitive(String string) {
+
+        String reversedString = "";
+
+        for (int i = string.length() - 1; i >= 0; i--) {
+            reversedString += string.toLowerCase().charAt(i);
+        }
+
+        System.out.println(reversedString);
+
+        if (string.toLowerCase().equals(reversedString)) {
+            System.out.println("This string is a palindrome!");
+        } else {
+            System.out.println("This string is not a palindrome!");
+        }
+
+    }
+
+    public void checkStringArray(String[] stringArray) {
+
+        int totalPalindromes = 0;
+
+        for (String string : stringArray) {
+
+            String reversedString = "";
+
+            for (int i = string.length() - 1; i >= 0; i--) {
+                reversedString += string.toLowerCase().charAt(i);
+            }
+
+            if (string.equals(reversedString)) {
+                System.out.println("Element " + (Arrays.asList(stringArray).indexOf(string) + 1) +
+                        " of array contains a palindrome: " + string);
+                totalPalindromes += 1;
+            }
 
         }
 
-        return result;
+        if (totalPalindromes == 0) {
+            System.out.println("The array does not contain any palindromes!");
+        }
+
     }
 
 }
