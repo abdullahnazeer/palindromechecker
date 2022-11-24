956cfd7e074d183a00a8c2d8e483987b07d75b65
Abdullah <abdullahnazeer22@gmail.com>
2022-11-24 17:55:58 +0000

    test-commit

---
+++ src/main/java/com/sparta/Main.java
@@ -0,0 +1,12 @@
+package com.sparta;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        PalindromeChecker checker = new PalindromeChecker();
+
+        checker.checkString("monsoon");
+
+    }
+}
---
+++ src/main/java/com/sparta/PalindromeChecker.java
@@ -0,0 +1,18 @@
+package com.sparta;
+
+public class PalindromeChecker {
+
+    public String checkString(String string) {
+
+        String result = "";
+
+        String reversedString = "";
+
+        for (int i = 0; i < string.length(); i++) {
+
+        }
+
+        return result;
+    }
+
+}
